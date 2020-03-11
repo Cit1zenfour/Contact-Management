4ae491fb6fc6d394bedcd77923cfe7e4c2d30865
westcoast9810@gmail.com <westcoast9810@gmail.com>
2020-03-11 23:43:36 +0500

    First modify

--- ContactManagement/Controllers/HomeController.cs
+++ ContactManagement/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContactManagement;
 
 namespace ContactManagement.Controllers
 {
@@ -15,7 +16,7 @@ namespace ContactManagement.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "";
 
             return View();
         }
